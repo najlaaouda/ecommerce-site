06daef4b944234c40cf5cae05b6b9763ebf64706
najlaaouda <najlaaouda.88@gmail.com>
2021-06-11 22:12:30 +0300

    upload singleproduct

--- js/javascript.js
+++ js/javascript.js
@@ -278,5 +278,16 @@ $(function() {
 
 })();
 
-
+function startRating(item){
+  rate=item.id[0];
+sessionStorage.star = rate;
+for(var i=0;i<5;i++){
+if(i<rate){
+document.getElementById((i+1)).style.color= "#FFC600";
+}
+else{
+document.getElementById((i+1)).style.color="#cccccc";
+}
+}
+}
 
